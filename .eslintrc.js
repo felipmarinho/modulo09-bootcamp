05b38c73493178fccd1eb36418a031a0b3b70abb
Felipe Marinho <felipmarinho@gmail.com>
2019-12-16 07:26:53 -0300

    Configurando as rotas

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,11 +34,4 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
-  settings: {
-    "import/resolver": {
-      "babel-plugin-root-import": {
-        rootPathSuffix: "src"
-      },
-    },
-  },
 };
